d8a828ee682aee65d0e3bfec3946c9f3c74c3a81
Ryan 'Thor' Kirkland <ryankirklandga@gmail.com>
2016-11-30 16:13:08 -0800

    Assignment User Model: completed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,13 @@
 class User < ActiveRecord::Base
   before_save { self.email = email.downcase if email.present? }
 
-# #3
+  before_save :format_name
+
   validates :name, length: { minimum: 1, maximum: 100 }, presence: true
-# #4
+
  validates :password, presence: true, length: { minimum: 6 }, if: "password_digest.nil?"
  validates :password, length: { minimum: 6 }, allow_blank: true
-# #5
+
   validates :email,
             presence: true,
             uniqueness: { case_sensitive: false },
@@ -14,4 +15,15 @@ class User < ActiveRecord::Base
 
 # #6
   has_secure_password
+
+  def format_name
+    if name
+      a = []
+      name.split.each do |name|
+        a << name.capitalize
+      end
+
+      self.name = a.join(" ")
+    end
+  end
 end
