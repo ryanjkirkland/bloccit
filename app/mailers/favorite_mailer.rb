1358b630e9b6bf88656381b36c5a406e41a5c74b
Ryan 'Thor' Kirkland <ryankirklandga@gmail.com>
2016-12-11 17:36:39 -0800

    Assignment Favorite: completed

--- app/mailers/favorite_mailer.rb
+++ app/mailers/favorite_mailer.rb
@@ -12,4 +12,14 @@ class FavoriteMailer < ApplicationMailer
 
     mail(to: user.email, subject: "New comment on #{post.title}")
   end
+
+  def new_post(post)
+    headers["Message-ID"] = "<posts/#{post.id}@lit-shelf-17597.herokuapp.com"
+    headers["In-Reply-To"] = "<post/#{post.id}@lit-shelf-17597.herokuapp.com"
+    headers["References"] = "<post/#{post.id}@lit-shelf-17597.herokuapp.com"
+
+    @post = post
+
+    mail(to: post.user.email, subject: "You're now following, #{post.title}!")
+  end
 end
