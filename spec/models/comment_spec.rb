4d1a55939133380a342d821053f531a79ae7f9c0
Ryan 'Thor' Kirkland <ryankirklandga@gmail.com>
2016-12-13 17:24:56 -0800

    Checkpoint Profile: completed

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  let(:topic) { Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph) }
-  let(:user) { User.create!(name: "Bloccit User", email: "user@bloccit.com")}
-  let(:post) { topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph, user: user) }
-  let(:comment) { Comment.create!(body: "Comment Body", post: post, user: user) }
+  let(:topic) { create(:topic) }
+  let(:user) { create(:user) }
+  let(:post) { create(:post) }
+  let(:comment) { Comment.create!(body: 'Comment Body', post: post, user: user) }
 
   it { is_expected.to belong_to(:post) }
   it { is_expected.to belong_to(:user) }
@@ -14,7 +14,7 @@ RSpec.describe Comment, type: :model do
 
   describe "attributes" do
     it "has a body attribute" do
-      expect(comment).to have_attribute(body: "Comment Body")
+      expect(comment).to have_attributes(body: "Comment Body")
     end
   end
 
