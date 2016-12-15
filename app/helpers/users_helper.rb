b129568dd4b655407019726b4177c70b3ac84ea3
Ryan 'Thor' Kirkland <ryankirklandga@gmail.com>
2016-12-15 15:29:35 -0800

    Assignment 32: completed

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,13 @@
 module UsersHelper
+  def has_no_posts?
+    @user.posts.count == 0
+  end
+
+  def has_no_comments?
+    @user.comments.count == 0
+  end
+
+  def has_no_favorites?
+    @user.favorites.count == 0
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
   has_many :comments, dependent: :destroy
   has_many :votes, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  has_many :favorited_posts, through: :favorites, source: :post
 
   before_save { self.email = email.downcase if email.present? }
   before_save { self.role ||= :member }
@@ -28,5 +29,5 @@ class User < ActiveRecord::Base
   def avatar_url(size)
     gravatar_id = Digest::MD5::hexdigest(self.email).downcase
     "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}"
-  end 
+  end
 end
